b1cefdf25f7837e7a3da2a0424d5e4ee2a517772
artichokeee <rusau.qalife@gmail.com>
2023-11-30 16:13:49 +0300

    added cypress and text

---
+++ cypress/e2e/3-verifier/page.cy.js
@@ -0,0 +1,10 @@
+describe('example to-do app', () => {
+    beforeEach(() => {
+      cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com')
+    })
+  
+    it('displays tasks list by default', () => {
+      cy.get('#task-heading').should('have.text', 'Tasks')
+    })
+  })
+  
