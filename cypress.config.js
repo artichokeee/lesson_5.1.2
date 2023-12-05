b221405052d9071a1153755af5153247e985d70c
artichokeee <rusau.qalife@gmail.com>
2023-12-06 00:37:40 +0300

    added cypress dashboard config hw 5.2.1

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "pvv2d9",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
