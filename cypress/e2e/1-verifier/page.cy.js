4c10b18fd8d103e1302612781b3ce96ea69d00a2
artichokeee <rusau.qalife@gmail.com>
2023-12-08 14:00:38 +0300

    added custom commands in specs hw5.3.1

--- cypress/e2e/1-verifier/page.cy.js
+++ cypress/e2e/1-verifier/page.cy.js
@@ -1,12 +1,9 @@
-describe('the main page is loaded', () => {
-    beforeEach(() => {
-      cy.visit('')
-    })
-  
-    it('displays tasks list by default', () => {
-      cy.get('#task-heading').should('have.text', 'Tasks')
-    })
-  })
-  
+describe("the main page is loaded", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
 
-  
+  it("displays tasks list by default", () => {
+    cy.checkText("#task-heading", "Tasks");
+  });
+});
